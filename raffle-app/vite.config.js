eedb1d4dc0d136786798055ac251734a7d666ba4
pasaoyu <pasaoyu@gmail.com>
2025-03-27 14:42:38 +0000

    [add] 初期コミット

---
+++ raffle-app/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/raffle-app-container/',  // GitHubリポジトリ名
+  server: {
+    host: true,
+    port: 5173,
+  },
+})
